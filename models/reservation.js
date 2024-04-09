e78bfb936f2df06237fac0d65e3742646a0c10f4
diamah06 <tabalmahdia@yahoo.fr>
2024-04-09 17:16:01 +0200

    add Express validator

--- models/reservation.js
+++ models/reservation.js
@@ -3,7 +3,12 @@ const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Reservation extends Model {
     static associate(models) {
+      // Association avec la table Users pour représenter l'utilisateur associé à la réservation
+       Reservation.belongsTo(models.User, { foreignKey: 'userId' });
+       // Association avec la table Spots via la table de liaison ReservationSpots
         Reservation.belongsToMany(models.Spot, { through: 'ReservationSpots', foreignKey: 'ReservationId'});
+        // Association avec la table Rooms via la clé étrangère roomId
+      Reservation.belongsTo(models.Room, { foreignKey: 'roomId' });
     }
   }
   
@@ -15,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
       status: DataTypes.INTEGER,
       userId: DataTypes.INTEGER,
       spotId: DataTypes.INTEGER,
-      roomId: DataTypes.INTEGER,
+      roomId: DataTypes.INTEGER,// Clé étrangère vers la table Rooms
     },
     {
       sequelize,
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,17 +1,54 @@
-const express = require("express");
+
+const express = require('express');
 const router = express.Router();
-const { User } = require("../utils/db");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const SECRET_KEY = "secretkey23456";
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {signUpValidationRules, signInValidationRules} = require('../middlewares/authValidationRules');
+const { validationResult } = require('express-validator');
 const authController = require('../controllers/authController');
 
-router.post("/signup", authController.signup);
+const SECRET_KEY = 'secretkey23456';
 
-router.post("/signin", authController.signin);
+// Simuler une base de données
+const users = [];
+
+// Sign-up (Inscription)
+router.post('/signup', signUpValidationRules(), async (req, res) => {
+  const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+
+    authController.signUp(req, res);
+});
+
+// Sign-in (Connexion)
+router.post('/signin', async (req, res) => {
+  const user = users.find(u => u.username === req.body.username);
+  if (!user) return res.status(400).send('Nom d\'utilisateur ou mot de passe incorrect');
+  
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send('Nom d\'utilisateur ou mot de passe incorrect');
+
+  const payload = {
+    username: user.username,
+    // Vous pouvez ajouter d'autres propriétés ici
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
+  res.json({message: token});
+});
+
+// auth
+router.get('/auth', (req, res) => {
+  const payload = {
+    username: user.username,
+  }
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: 60 * 60 * 24 });
+  res.json({ auth: true, token });
+});
 
 // Route pour réinitialiser le mot de passe
 router.post("/reset-password", authController.resetPassword);
 
 module.exports = router;
-
